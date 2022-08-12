524244e1e27b058d6380ac5e7e03b12e1c9fb1f4
unknown <tmrafinha4@gmail.com>
2022-08-12 15:20:15 -0300

    seventime

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Flex w="100%" mt="6" maxWidth={1600} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" height={"100%"} borderRadius={8} p="8" maxW={700}>
+        <Box flex="1" height={"100%"} borderRadius={8} p="8" maxW={1200}>
           <Flex>
             <Box w={"60%"} >
               <Text fontSize={"6xl"} color="gray.100" fontWeight="semibold">
