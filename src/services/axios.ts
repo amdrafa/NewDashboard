2bbf1cc2793ac09b9b91c089bb7bde63a30eb922
unknown <tmrafinha4@gmail.com>
2023-01-26 14:50:30 -0300

    newmod

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { parseCookies } from 'nookies'
 
-let {auth} = parseCookies()
+let { auth } = parseCookies()
 
 export const api = axios.create({
-    // baseURL: 'https://localhost:3000/api',
-    baseURL: 'https://ctvi.vercel.app/api',
+    baseURL: 'https://localhost:3000/api',
+    // baseURL: 'https://ctvi.vercel.app/api',
     headers: {
-        authorization: (auth? auth :- '')
+        authorization: (auth ? auth : - '')
     }
 })
